008a66223e7e93aab476b7c8525ca16f127ee580
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-19 18:40:09 +0500

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
