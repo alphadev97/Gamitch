5dbb8c8958c3fce00407a2fb662ae891939d7654
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-19 18:12:33 +0500

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const navIcons = [
+  {
+    src: "/assets/icons/search.svg",
+    alt: "search",
+  },
+  {
+    src: "/assets/icons/black-heart.svg",
+    alt: "heart",
+  },
+  {
+    src: "/assets/icons/user.svg",
+    alt: "user",
+  },
+];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href={"/"} className="flex items-center gap-1">
+          <Image
+            src={"/assets/icons/logo.png"}
+            width={27}
+            height={27}
+            alt="logo"
+          />
+
+          <p className="nav-logo">
+            Price<span className="text-primary">Alpha97</span>
+          </p>
+        </Link>
+
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className="object-contain"
+            />
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
