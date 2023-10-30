a68af9547f22f222f8adaa2274eb71b43f34e1df
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-30 20:21:04 +0500

    FollowBar

--- components/Layout/FollowBar.tsx
+++ components/Layout/FollowBar.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const FollowBar = () => {
   return (
-    <div>FollowBar</div>
-  )
-}
+    <div className="px-6 py-4 hidden lg:block">
+      <div className="bg-neutral-800 rounded-xl p-4">
+        <h2 className="text-white text-xl font-semibold">Who to follow</h2>
+      </div>
+    </div>
+  );
+};
 
-export default FollowBar
+export default FollowBar;
