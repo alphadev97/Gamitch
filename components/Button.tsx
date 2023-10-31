605c2bc578e1bc5e3853ab759b3cc8e79b294cba
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-31 17:14:16 +0500

    Button Interface

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+interface ButtonProps {
+  label: string;
+  secondary?: boolean;
+  fullWidth?: boolean;
+  large?: boolean;
+  onClick: () => void;
+  disabled?: boolean;
+  outline?: boolean;
+}
 
-const Button = () => {
+const Button: React.FC<ButtonProps> = ({
+  label,
+  secondary,
+  fullWidth,
+  large,
+  onClick,
+  disabled,
+  outline,
+}) => {
   return <div>Button</div>;
 };
 
