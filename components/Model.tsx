39401ca5513057bfb99c1032180122f1b6b6865e
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-10-31 18:16:58 +0500

    Model - Components

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,6 +1,44 @@
-import React from "react";
+import { useCallback } from "react";
 
-const Model = () => {
+interface ModelProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+}
+
+const Model: React.FC<ModelProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+}) => {
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onClose();
+  }, [disabled, onClose]);
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  if (!isOpen) {
+    return null;
+  }
+
   return <div>Model</div>;
 };
 
