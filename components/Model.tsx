bfab56441f63f75d48ba17a805af20e41dd31e8c
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-01 16:59:03 +0500

    Model - Components

--- components/Model.tsx
+++ components/Model.tsx
@@ -43,7 +43,15 @@ const Model: React.FC<ModelProps> = ({
     <div>
       <>
         <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800 bg-opacity-70">
-          <div className="relative w-full lg:w-3/6 my-6 mx-auto lg:max-w-3xl h-full lg:h-auto"></div>
+          <div className="relative w-full lg:w-3/6 my-6 mx-auto lg:max-w-3xl h-full lg:h-auto">
+            {/* Content */}
+            <div className="h-full lg:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-black outline-none focus:outline-none">
+              {/* Header */}
+              <div className="flex items-center justify-between p-10 rounded">
+                <h3 className="text-3xl font-semibold text-white">{title}</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     </div>
