b2c91aeb9d6320e9e5e1325b89fc80a31a14158f
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-27 18:10:18 +0500

    LoginModel

--- components/Models/LoginModel.tsx
+++ components/Models/LoginModel.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import useLoginModel from "@/hooks/useLoginModel";
+import React, { useCallback, useState } from "react";
 
 const LoginModel = () => {
+  const loginModel = useLoginModel();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(() => {}, []);
+
   return <div>LoginModel</div>;
 };
 
