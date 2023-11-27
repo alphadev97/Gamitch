bc9eb1ce24b38f351f66c662d07e85149c93a7bb
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-27 18:10:27 +0500

    LoginModel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Layout from "@/components/Layout";
+import LoginModel from "@/components/Models/LoginModel";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <>
+          <LoginModel />
           <Layout>{children}</Layout>
         </>
       </body>
