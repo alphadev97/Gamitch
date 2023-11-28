25b82a8a09b295a6011d290357f48103c717bb05
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-28 19:34:09 +0500

    Tweet Onclick Login

--- components/Layout/SidebarTweetButton.tsx
+++ components/Layout/SidebarTweetButton.tsx
@@ -1,13 +1,20 @@
 "use client";
 
+import useLoginModel from "@/hooks/useLoginModel";
 import { useRouter } from "next/navigation";
+import { useCallback } from "react";
 import { FaFeather } from "react-icons/fa";
 
 const SidebarTweetButton = () => {
   const router = useRouter();
+  const loginModel = useLoginModel();
+
+  const onClick = useCallback(() => {
+    loginModel.onOpen();
+  }, [loginModel]);
 
   return (
-    <div onClick={() => router.push("/")}>
+    <div onClick={onClick}>
       <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
         <FaFeather size={24} color="white" />
       </div>
