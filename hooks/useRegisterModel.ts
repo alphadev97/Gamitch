836fb3190f30e92c37bf4925b118ebfd16ba9dd3
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-28 19:08:57 +0500

    useRegisterModel

--- hooks/useRegisterModel.ts
+++ hooks/useRegisterModel.ts
@@ -1,15 +1,15 @@
 import { create } from "zustand";
 
-interface LoginModelStore {
+interface RegisterModelStore {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
 }
 
-const useLoginModel = create<LoginModelStore>((set) => ({
+const useRegisterModel = create<RegisterModelStore>((set) => ({
   isOpen: true,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
 }));
 
-export default useLoginModel;
+export default useRegisterModel;
