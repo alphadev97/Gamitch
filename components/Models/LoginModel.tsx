469b5d04f3d4577dc32b0b2749de1bef31050539
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-28 19:06:16 +0500

    LoginModel

--- components/Models/LoginModel.tsx
+++ components/Models/LoginModel.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import useLoginModel from "@/hooks/useLoginModel";
 import React, { useCallback, useState } from "react";
+import Input from "../Input";
+import Model from "../Model";
 
 const LoginModel = () => {
   const loginModel = useLoginModel();
@@ -8,9 +12,48 @@ const LoginModel = () => {
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const onSubmit = useCallback(() => {}, []);
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
 
-  return <div>LoginModel</div>;
+      // TODO ADD LOG IN
+
+      loginModel.onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [loginModel]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        disabled={isLoading}
+      />
+    </div>
+  );
+
+  return (
+    <Model
+      disabled={isLoading}
+      isOpen={loginModel.isOpen}
+      title="Login"
+      actionLabel="Sign in"
+      onClose={loginModel.onClose}
+      onSubmit={onSubmit}
+      body={bodyContent}
+    />
+  );
 };
 
 export default LoginModel;
