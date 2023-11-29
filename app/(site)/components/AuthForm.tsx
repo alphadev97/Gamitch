7afd8dd9e2dd54fe80b6a9e14dd0bb48b687a807
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-11-29 22:17:30 +0500

    AuthForm

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      // Axios register
+    }
+
+    if (variant === "LOGIN") {
+      // Nextauth signin
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    // Nextauth Social Sign in
+  };
+
+  return <div>AuthForm!</div>;
+};
+
+export default AuthForm;
