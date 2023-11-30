813cab531acdec779aa2ebc3e5769d853bf66d3a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-12-01 00:15:55 +0500

    Sidebar

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
+
 import DesktopSidebar from "./DesktopSidebar";
 import MobileFooter from "./MobileFooter";
 
 async function Sidebar({ children }: { children: React.ReactNode }) {
-  const currentUser = getCurrentUser();
+  const currentUser = await getCurrentUser();
 
   return (
     <div className="h-full">
