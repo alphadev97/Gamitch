650c7ee6ade5c20566c5e950ed397ddff55eb4f0
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-12-01 20:41:01 +0500

    init

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-    swcPlugins: [["next-superjson-plugin", {}]],
-  },
-  images: {
-    domains: [
-      "res.cloudinary.com",
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com",
-    ],
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
