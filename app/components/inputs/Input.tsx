ec24441c3089354bbdb9e6b3bc34ce45d2b7541b
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-12-04 18:20:58 +0500

    Input Component

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,5 +1,46 @@
-const Input = () => {
-  return <div>Input</div>;
+"use client";
+
+import clsx from "clsx";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  label: string;
+  id: string;
+  type?: string;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  disabled?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({
+  label,
+  id,
+  type,
+  required,
+  register,
+  errors,
+  disabled,
+}) => {
+  return (
+    <div>
+      <label
+        htmlFor={id}
+        className="
+          block 
+          text-sm 
+          font-medium 
+          leading-6 
+          text-gray-900
+        "
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input />
+      </div>
+    </div>
+  );
 };
 
 export default Input;
