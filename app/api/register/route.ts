00fd582f9395e9acde3908b95b8f3d0ab326256f
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-08 20:00:02 +0500

    register route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -5,4 +5,8 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const body = await request.json();
   const { name, email, password } = body;
+
+  if (!email || !name || !password) {
+    return new NextResponse("Missing info", { status: 400 });
+  }
 }
