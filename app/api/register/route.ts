ad954813a8123e966fde3cb8af795f8c6acb1d54
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-08 19:56:56 +0500

    register route

---
+++ app/api/register/route.ts
@@ -0,0 +1,7 @@
+import bcrypt from "bcrypt";
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+}
