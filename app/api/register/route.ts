1a02a1f7eb7ff0e61c7a4e7d9d718b8304bf43cc
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-11 20:38:31 +0500

    Regiter Route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -3,22 +3,26 @@ import prisma from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const body = await request.json();
-  const { name, email, password } = body;
+  try {
+    const body = await request.json();
+    const { name, email, password } = body;
 
-  if (!email || !name || !password) {
-    return new NextResponse("Missing info", { status: 400 });
+    if (!email || !name || !password) {
+      return new NextResponse("Missing info", { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error: any) {
+    console.log(error, "REGISTRATION_ERROR");
   }
-
-  const hashedPassword = await bcrypt.hash(password, 12);
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      name,
-      hashedPassword,
-    },
-  });
-
-  return NextResponse.json(user);
 }
