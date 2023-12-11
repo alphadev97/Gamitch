c474b7724455347645ec459c24162031f7802121
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-11 20:17:56 +0500

    Register Route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -9,4 +9,16 @@ export async function POST(request: Request) {
   if (!email || !name || !password) {
     return new NextResponse("Missing info", { status: 400 });
   }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
 }
