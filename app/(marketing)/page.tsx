4c7f092f36af6026f43b84e401c4cf4ab9847e76
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-13 19:00:44 +0500

    Heading

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Heading } from "./_components/Heading";
 
-export default function Home() {
+const MarketingPage = () => {
   return (
-    <div>
-      <Button>Click Me</Button>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+      </div>
     </div>
   );
-}
+};
+
+export default MarketingPage;
