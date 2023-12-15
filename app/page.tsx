f234cc7dd3f4e19238aaa6b796fbd5e8ae960809
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-15 21:57:54 +0500

    UserButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <p>Only Authenticated User can see this</p>;
+import { UserButton } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
