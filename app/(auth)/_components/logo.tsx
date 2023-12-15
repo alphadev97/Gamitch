04e12f311c4ca8d351d8b010cf491efa495c5aab
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-15 23:37:03 +0500

    Logo Component

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return <div>Logo!</div>;
+};
