30e64d7bfc5cd5d6d17c8dbfd4aef393aa9f02e8
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-15 23:51:07 +0500

    Logo component

--- app/(auth)/_components/logo.tsx
+++ app/(auth)/_components/logo.tsx
@@ -8,5 +8,20 @@ const font = Poppins({
 });
 
 export const Logo = () => {
-  return <div>Logo!</div>;
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+      <div className="bg-white rounded-full p-1">
+        <Image
+          src={"/gamitch-logo.svg"}
+          alt="Gamitch"
+          height={"80"}
+          width={"80"}
+        />
+      </div>
+      <div className={cn("flex flex-col items-center", font.className)}>
+        <p className="text-xl font-semibold">Gamitch</p>
+        <p className="text-sm text-muted-foreground">Let&apos;s Play</p>
+      </div>
+    </div>
+  );
 };
