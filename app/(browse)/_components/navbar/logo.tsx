538b6fe04ef86aadb97656bb6ad2d4f23d7ca208
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-19 00:26:33 +0500

    logo responsive

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -11,8 +11,8 @@ const font = Poppins({
 export const Logo = () => {
   return (
     <Link href={"/"}>
-      <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
-        <div className="bg-white rounded-full p-1">
+      <div className="flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
           <Image
             src={"/gamitch-logo.svg"}
             alt="Gamitch"
@@ -20,7 +20,7 @@ export const Logo = () => {
             width={"32"}
           />
         </div>
-        <div className={cn(font.className)}>
+        <div className={cn("hidden lg:block", font.className)}>
           <p className="text-lg font-semibold">Gamitch</p>
           <p className="text-xs text-muted-foreground">Let&apos;s Play</p>
         </div>
