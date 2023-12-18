e724cd89b06ee591fc88effecda95732c2a4b6f4
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-18 21:45:57 +0500

    logo component for navbar

---
+++ app/(browse)/_components/navbar/logo.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <Link href={"/"}>
+      <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1">
+          <Image
+            src={"/gamitch-logo.svg"}
+            alt="Gamitch"
+            height={"32"}
+            width={"32"}
+          />
+        </div>
+        <div className={cn(font.className)}>
+          <p className="text-lg font-semibold">Gamitch</p>
+          <p className="text-xs text-muted-foreground">Let&apos;s Play</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
