fddaeda8a7fa4db2bde7f2a58d822c1340ea0580
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-18 22:49:12 +0500

    Search functionality

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -43,6 +43,12 @@ export const Search = () => {
         placeholder="Search"
         className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
       />
+      {value && (
+        <X
+          className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition"
+          onClick={onClear}
+        />
+      )}
       <Button
         type={"submit"}
         size={"sm"}
