a66bb01470cbf48eaa84b26107b496172fe46100
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-19 00:14:30 +0500

    Navbar Actions Component

--- app/(browse)/_components/navbar/actions.tsx
+++ app/(browse)/_components/navbar/actions.tsx
@@ -1,3 +1,35 @@
-export const Actions = () => {
-  return <div>Actions</div>;
+import { SignInButton, UserButton, currentUser } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Clapperboard } from "lucide-react";
+
+export const Actions = async () => {
+  const user = await currentUser();
+
+  return (
+    <div className="flex items-center justify-end gap-x-2 ml-4">
+      {!user && (
+        <SignInButton>
+          <Button>Login</Button>
+        </SignInButton>
+      )}
+      {!!user && (
+        <div className="flex items-center ga-x-4">
+          <Button
+            size={"sm"}
+            variant={"ghost"}
+            className="text-muted-foreground hover:text-primary"
+            asChild
+          >
+            <Link href={`/u/${user.username}`}>
+              <Clapperboard className="h-5 w-5 lg:mr-2" />
+              <span className="hidden lg:block">Dashboard</span>
+            </Link>
+          </Button>
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      )}
+    </div>
+  );
 };
