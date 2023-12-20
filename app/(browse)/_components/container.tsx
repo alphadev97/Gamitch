9d53816f938fd61601b52bc62e39335ef28ee66a
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-20 18:39:02 +0500

    Container component

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useEffect } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
 
@@ -8,7 +11,16 @@ interface ContainerProps {
 }
 
 export const Container = ({ children }: ContainerProps) => {
-  const { collapsed } = useSidebar((state) => state);
+  const matches = useMediaQuery("(max-width: 1024px)");
+  const { collapsed, onCollapse, onExpand } = useSidebar((state) => state);
+
+  useEffect(() => {
+    if (matches) {
+      onCollapse();
+    } else {
+      onExpand();
+    }
+  }, [matches, onCollapse, onExpand]);
 
   return (
     <div
