f38b3d7ebadf17794e7f9b7c38a7d91418489c98
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-20 17:37:32 +0500

    Collapse Toggle

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/store/use-sidebar";
-import { ArrowLeftFromLine } from "lucide-react";
+import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
 
 export const Toggle = () => {
   const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
@@ -12,6 +12,14 @@ export const Toggle = () => {
   console.log("i am logged here");
   return (
     <>
+      {collapsed && (
+        <div className="hidden lg:flex w-full items-center justify-center pt-4 mb-4">
+          <Button onClick={onExpand} variant={"ghost"} className="h-auto p-2">
+            <ArrowRightFromLine className="h-4 w-4" />
+          </Button>
+        </div>
+      )}
+
       {!collapsed && (
         <div className="p-3 pl-6 mb-2 flex items-center w-full">
           <p className="font-semibold text-primary">For you</p>
