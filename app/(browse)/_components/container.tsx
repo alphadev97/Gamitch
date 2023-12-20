5e165a8a646d13cfdb024bffde2f8d7d82c3aec2
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-20 18:11:15 +0500

    Container component

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useSidebar } from "@/store/use-sidebar";
+
 interface ContainerProps {
   children: React.ReactNode;
 }
 
 export const Container = ({ children }: ContainerProps) => {
-  return <div>{children}</div>;
+  const { collapsed } = useSidebar((state) => state);
+
+  return (
+    <div
+      className={cn("flex-1", collapsed ? "ml-[70px]" : "ml-[70px] lg:ml-60")}
+    >
+      {children}
+    </div>
+  );
 };
