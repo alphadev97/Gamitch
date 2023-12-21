cb056823cc3ab40e02d2dc19c488a7a7f1ec19a4
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-21 18:56:03 +0500

    add recommanded component to sidebar

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -5,6 +5,9 @@ export const Sidebar = () => {
   return (
     <Wrapper>
       <Toggle />
+      <div className="space-y-4 pt-4 lg:pt-0">
+        <Recommanded />
+      </div>
     </Wrapper>
   );
 };
