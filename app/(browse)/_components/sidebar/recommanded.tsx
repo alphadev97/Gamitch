5d803644013da28862b0bc1f8beaa1e30926fd6c
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-21 19:45:46 +0500

    recommanded user

--- app/(browse)/_components/sidebar/recommanded.tsx
+++ app/(browse)/_components/sidebar/recommanded.tsx
@@ -1,9 +1,36 @@
+"use client";
+
+import { useSidebar } from "@/store/use-sidebar";
 import { User } from "@prisma/client";
+import { UserItem } from "./user-item";
 
 interface RecommandedProps {
   data: User[];
 }
 
 export const Recommanded = ({ data }: RecommandedProps) => {
-  return <div>Recommanded!</div>;
+  const { collapsed } = useSidebar((state) => state);
+
+  const showLabel = !collapsed && data.length > 0;
+
+  return (
+    <div>
+      {showLabel && (
+        <div className="pl-6 mb-4">
+          <p className="text-sm text-muted-foreground">Recommanded</p>
+        </div>
+      )}
+
+      <ul className="space-y-2 px-2">
+        {data.map((user) => (
+          <UserItem
+            key={user.id}
+            username={user.username}
+            imageUrl={user.imageUrl}
+            isLive={true}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
