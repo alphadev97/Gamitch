1bbb345fbaf36154bca7c73a9c0b7270a376ddeb
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-21 19:57:06 +0500

    user item comp

--- app/(browse)/_components/sidebar/user-item.tsx
+++ app/(browse)/_components/sidebar/user-item.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/store/use-sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
+
 interface UserItemProps {
   username: string;
   imageUrl: string;
@@ -7,5 +14,31 @@ interface UserItemProps {
 }
 
 export const UserItem = ({ username, imageUrl, isLive }: UserItemProps) => {
-  return <div>User Item</div>;
+  const pathname = usePathname();
+  const { collapsed } = useSidebar((state) => state);
+  const href = `/${username}`;
+  const isActive = pathname === href;
+
+  return (
+    <Button
+      asChild
+      variant={"ghost"}
+      className={cn(
+        "w-full h-12",
+        collapsed ? "justify-center" : "justify-start",
+        isActive && "bg-accent"
+      )}
+    >
+      <Link href={href}>
+        <div
+          className={cn(
+            "flex items-center w-full gap-x-4",
+            collapsed && "justify-center"
+          )}
+        >
+          <UserAvatar imageUrl={imageUrl} username={username} isLive={isLive} />
+        </div>
+      </Link>
+    </Button>
+  );
 };
