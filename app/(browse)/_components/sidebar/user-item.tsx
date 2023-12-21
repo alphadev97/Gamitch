b1396c489d8a0271aabac3544bc1e2c375284543
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-21 19:45:55 +0500

    user item comp

---
+++ app/(browse)/_components/sidebar/user-item.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+interface UserItemProps {
+  username: string;
+  imageUrl: string;
+  isLive?: boolean;
+}
+
+export const UserItem = ({ username, imageUrl, isLive }: UserItemProps) => {
+  return <div>User Item</div>;
+};
