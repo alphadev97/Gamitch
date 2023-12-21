55a82518a322a0c58c2b8da6acf6213a19573a28
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-21 18:59:55 +0500

    Pass the Recommanded user data to reommanded comp via props

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,13 +1,16 @@
+import { getRecommanded } from "@/lib/recommanded-service";
 import { Recommanded } from "./recommanded";
 import { Toggle } from "./toggle";
 import { Wrapper } from "./wrapper";
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const recommanded = await getRecommanded();
+
   return (
     <Wrapper>
       <Toggle />
       <div className="space-y-4 pt-4 lg:pt-0">
-        <Recommanded />
+        <Recommanded data={recommanded} />
       </div>
     </Wrapper>
   );
