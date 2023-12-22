6c9dec2106280f08aec341dfc500f8a81dbb83b7
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-22 23:47:54 +0500

    user page

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,11 +1,29 @@
+import { isFollowingUser } from "@/lib/follow-service";
+import { getUserByUsername } from "@/lib/user-service";
+import { notFound } from "next/navigation";
+
 interface UserPageProps {
   params: {
     username: string;
   };
 }
 
-const UserPage = ({ params }: UserPageProps) => {
-  return <div>User: {params.username}</div>;
+const UserPage = async ({ params }: UserPageProps) => {
+  const user = await getUserByUsername(params.username);
+
+  if (!user) {
+    notFound();
+  }
+
+  const isFollowing = await isFollowingUser(user.id);
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <p>Username: {user.username}</p>
+      <p>User ID: {user.id}</p>
+      <p>Is Following: {`${isFollowing}`}</p>
+    </div>
+  );
 };
 
 export default UserPage;
