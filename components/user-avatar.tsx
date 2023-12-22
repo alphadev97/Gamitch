52201b525318aa886517d0ae3710c7f3da31f2e5
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-22 18:35:55 +0500

    User avatar component

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -2,6 +2,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LiveBadge } from "./live-badge";
 
 const avatarSizes = cva("", {
   variants: {
@@ -35,11 +36,21 @@ export const UserAvatar = ({
     <div className="relative">
       <Avatar
         className={cn(
-          isLive && "ring-2 ring-rose-500 border border-background"
+          isLive && "ring-2 ring-rose-500 border border-background",
+          avatarSizes({ size })
         )}
       >
         <AvatarImage src={imageUrl} className="object-cover" />
+        <AvatarFallback>
+          {username[0]}
+          {username[username.length - 1]}
+        </AvatarFallback>
       </Avatar>
+      {canShowBadge && (
+        <div className="absolute -bottom-3 left-1/2 transform -translate-x-1/2">
+          <LiveBadge />
+        </div>
+      )}
     </div>
   );
 };
