b85f5d160567ae4f2d3db8ba724fd26d352f76be
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-23 00:34:50 +0500

    Follow user

--- actions/follow.ts
+++ actions/follow.ts
@@ -1,8 +1,18 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+import { followUser } from "@/lib/follow-service";
+
 export const onFollow = async (id: string) => {
   try {
-    console.log("I am the same as an api call", id);
+    const followedUser = await followUser(id);
+    revalidatePath("/");
+
+    if (followedUser) {
+      revalidatePath(`/${followedUser.following.username}`);
+    }
+
+    return followedUser;
   } catch (error) {
     throw new Error("internal error");
   }
--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -3,17 +3,23 @@
 import { onFollow } from "@/actions/follow";
 import { Button } from "@/components/ui/button";
 import { useTransition } from "react";
+import { toast } from "sonner";
 
 interface ActionProps {
   isFollowing: boolean;
+  userId: string;
 }
 
-export const Actions = ({ isFollowing }: ActionProps) => {
+export const Actions = ({ isFollowing, userId }: ActionProps) => {
   const [isPending, startTransition] = useTransition();
 
   const onClick = () => {
     startTransition(() => {
-      onFollow("123");
+      onFollow(userId)
+        .then((data) =>
+          toast.success(`You are now following ${data.following.username}`)
+        )
+        .catch(() => toast.error("Something went wrong"));
     });
   };
 
