3e98be5b6c326c6d141621c5b5669263eca91524
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-22 20:44:23 +0500

    Hydration Error Problem Solved
    For more depth information about Hydration error, you can check out this article for more better understanding, i found this article very helpful
    https://www.joshwcomeau.com/react/the-perils-of-rehydration/

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,6 +1,6 @@
 import { getRecommanded } from "@/lib/recommanded-service";
 import { Recommanded, RecommandedSkeleton } from "./recommanded";
-import { Toggle } from "./toggle";
+import { Toggle, ToggleSkeleton } from "./toggle";
 import { Wrapper } from "./wrapper";
 
 export const Sidebar = async () => {
@@ -19,6 +19,7 @@ export const Sidebar = async () => {
 export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+      <ToggleSkeleton />
       <RecommandedSkeleton />
     </aside>
   );
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -2,18 +2,34 @@
 
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+import { useState, useEffect } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommandedSkeleton } from "./recommanded";
 
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export const Wrapper = ({ children }: WrapperProps) => {
+  const [isClient, setIsClient] = useState(false);
   const { collapsed } = useSidebar((state) => state);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient)
+    return (
+      <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+        <ToggleSkeleton />
+        <RecommandedSkeleton />
+      </aside>
+    );
 
   return (
     <aside
       className={cn(
-        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+        "fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50",
         collapsed && "w-[70px]"
       )}
     >
