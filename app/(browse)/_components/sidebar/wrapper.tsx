3e7ffec313bb8184475cfa214d6dd0d842cabee2
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-26 19:02:29 +0500

    Sidebar wrapper

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -5,6 +5,7 @@ import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
 import { ToggleSkeleton } from "./toggle";
 import { RecommandedSkeleton } from "./recommanded";
+import { FollowingSekeleton } from "./following";
 
 interface WrapperProps {
   children: React.ReactNode;
@@ -18,6 +19,7 @@ export const Wrapper = ({ children }: WrapperProps) => {
     return (
       <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
         <ToggleSkeleton />
+        <FollowingSekeleton />
         <RecommandedSkeleton />
       </aside>
     );
