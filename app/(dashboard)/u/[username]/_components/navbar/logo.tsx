d1b17aeaf26d4a6a91b448cea796acc38400da95
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-27 01:35:54 +0500

    Creator page with navbar

---
+++ app/(dashboard)/u/[username]/_components/navbar/logo.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <Link href={"/"}>
+      <div className="flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink">
+          <Image
+            src={"/gamitch-logo.svg"}
+            alt="Gamitch"
+            height={"32"}
+            width={"32"}
+          />
+        </div>
+        <div className={cn("hidden lg:block", font.className)}>
+          <p className="text-lg font-semibold">Gamitch</p>
+          <p className="text-xs text-muted-foreground">Let&apos;s Play</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
