b9a0701af19caa931dd4a6c9bfe032c910deb450
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-26 22:07:07 +0500

    Block user functionality

--- lib/block-service.ts
+++ lib/block-service.ts
@@ -33,3 +33,46 @@ export const isBlockedByUser = async (id: string) => {
     return false;
   }
 };
+
+export const blockUser = async (id: string) => {
+  const self = await getSelf();
+
+  if (self.id === id) {
+    throw new Error("Cannout block yourself");
+  }
+
+  const otherUser = await db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!otherUser) {
+    throw new Error("User not found");
+  }
+
+  const existingBlock = await db.block.findUnique({
+    where: {
+      blockerId_blockedId: {
+        blockerId: self.id,
+        blockedId: otherUser.id,
+      },
+    },
+  });
+
+  if (existingBlock) {
+    throw new Error("Already blocked");
+  }
+
+  const block = await db.block.create({
+    data: {
+      blockerId: self.id,
+      blockedId: otherUser.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return block;
+};
