361fee77dd20c59aa11bbe2a9bafe9473c0c3ca2
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-27 00:40:21 +0500

    Block

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -44,9 +44,9 @@ export const Actions = ({ isFollowing, userId }: ActionProps) => {
 
   const handleBlock = () => {
     startTransition(() => {
-      onUnblock(userId)
+      onBlock(userId)
         .then((data) =>
-          toast.success(`Unblocked the user ${data.blocked.username}`)
+          toast.success(`Blocked the user ${data.blocked.username}`)
         )
         .catch(() => toast.error("Something went wrong!"));
     });
@@ -58,7 +58,7 @@ export const Actions = ({ isFollowing, userId }: ActionProps) => {
         {isFollowing ? "Unfollow" : "Follow"}
       </Button>
       <Button onClick={handleBlock} disabled={isPending}>
-        Unblock
+        Block
       </Button>
     </>
   );
