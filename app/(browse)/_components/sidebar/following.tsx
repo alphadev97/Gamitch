cbfea12e9135c48ef935dbca625b4e12981bc242
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-26 18:55:24 +0500

    Following Sidebar

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -2,6 +2,7 @@
 
 import { useSidebar } from "@/store/use-sidebar";
 import { Follow, User } from "@prisma/client";
+import { UserItem } from "./user-item";
 
 interface FollowingProps {
   data: (Follow & { following: User })[];
@@ -21,6 +22,17 @@ export const Following = ({ data }: FollowingProps) => {
           <p className="text-sm text-muted-foreground">Following</p>
         </div>
       )}
+
+      <ul className="space-y-2 px-2">
+        {data.map((follow) => (
+          <UserItem
+            key={follow.following.id}
+            username={follow.following.username}
+            imageUrl={follow.following.imageUrl}
+            isLive={true}
+          />
+        ))}
+      </ul>
     </div>
   );
 };
