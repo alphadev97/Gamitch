b8343468af5bd5bcb624a0ff5d3b1b38e9fdfa2d
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-27 00:53:26 +0500

    unBlock

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -44,7 +44,7 @@ export const Actions = ({ isFollowing, userId }: ActionProps) => {
 
   const handleBlock = () => {
     startTransition(() => {
-      onBlock(userId)
+      onUnblock(userId)
         .then((data) =>
           toast.success(`Blocked the user ${data.blocked.username}`)
         )
