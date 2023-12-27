ab1e31fdce76aa950ba528af0cd41f4a4b71368b
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-27 18:50:46 +0500

    store for creator dashboard

--- store/use-creator-sidebar.ts
+++ store/use-creator-sidebar.ts
@@ -1,12 +1,12 @@
 import { create } from "zustand";
 
-interface SidebarStore {
+interface CreatorSidebarStore {
   collapsed: boolean;
   onExpand: () => void;
   onCollapse: () => void;
 }
 
-export const useSidebar = create<SidebarStore>((set) => ({
+export const useCreatorSidebar = create<CreatorSidebarStore>((set) => ({
   collapsed: false,
   onExpand: () => set(() => ({ collapsed: false })),
   onCollapse: () => set(() => ({ collapsed: true })),
