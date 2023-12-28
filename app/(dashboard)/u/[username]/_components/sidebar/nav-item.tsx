f5ba461685aa174fe5d8abfdcde06c6ab83ca8e7
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-28 18:48:58 +0500

    nav item skeleton

--- app/(dashboard)/u/[username]/_components/sidebar/nav-item.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/nav-item.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 import { useCreatorSidebar } from "@/store/use-creator-sidebar";
 import { LucideIcon } from "lucide-react";
@@ -38,5 +39,16 @@ export const NavItem = ({
         </div>
       </Link>
     </Button>
+  );
+};
+
+export const NavItemSkeleton = () => {
+  return (
+    <li className="flex items-center gap-x-4 px-3 py-2">
+      <Skeleton className="min-h-[48px] min-w-[48px] rounded-md" />
+      <div className="flex-1 hidden lg:block">
+        <Skeleton className="h-6" />
+      </div>
+    </li>
   );
 };
