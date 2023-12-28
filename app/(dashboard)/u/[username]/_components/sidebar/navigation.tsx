767dcee734ba5cedf6cd3a4b3ca43bcab462fcbb
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-28 18:33:59 +0500

    creator sidebar navigation

--- app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
@@ -1,5 +1,47 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { Fullscreen, KeyRound, MessageSquare, Users } from "lucide-react";
+
 export const Navigation = () => {
-  return <div>Navigation</div>;
+  const pathname = usePathname();
+  const { user } = useUser();
+
+  const routes = [
+    {
+      label: "Stream",
+      href: `/u/${user?.username}`,
+      icon: Fullscreen,
+    },
+    {
+      label: "Keys",
+      href: `/u/${user?.username}/keys`,
+      icon: KeyRound,
+    },
+    {
+      label: "Chat",
+      href: `/u/${user?.username}/chat`,
+      icon: MessageSquare,
+    },
+    {
+      label: "Community",
+      href: `/u/${user?.username}/community`,
+      icon: Users,
+    },
+  ];
+
+  return (
+    <ul className="space-y-2 px-2 pt-4 lg:pt-0">
+      {routes.map((route) => (
+        <NavItem
+          key={route.href}
+          label={route.label}
+          icon={route.icon}
+          href={route.href}
+          isActive={pathname === route.href}
+        />
+      ))}
+    </ul>
+  );
 };
