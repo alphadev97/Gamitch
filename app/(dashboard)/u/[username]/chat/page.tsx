64494e4eb018e17dd3ae7e665f8a3c71297cb1b0
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-29 17:46:27 +0500

    Chat page

--- app/(dashboard)/u/[username]/chat/page.tsx
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,5 +1,11 @@
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-bold">Chat Settings</h1>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;
