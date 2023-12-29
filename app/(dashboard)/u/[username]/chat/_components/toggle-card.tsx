dc836ae0c1c75342396e889fdc58c9d655aa143b
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-29 18:17:41 +0500

    toggle card component

--- app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Switch } from "@/components/ui/switch";
+
 type FieldTypes = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly";
 
 interface ToggleCardProps {
@@ -13,5 +15,14 @@ export const ToggleCard = ({
   value = false,
   field,
 }: ToggleCardProps) => {
-  return <div>Toggle Card</div>;
+  return (
+    <div className="rounded-xl bg-muted p-6">
+      <div className="flex items-center justify-between">
+        <p className="font-semibold shrink-0">{label}</p>
+        <div className="space-y-2">
+          <Switch checked={value}>{value ? "On" : "Off"}</Switch>
+        </div>
+      </div>
+    </div>
+  );
 };
