c6b55a357bf364b8f1ead3c523123e68c9622840
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2023-12-29 18:53:12 +0500

    toggle card setting

--- app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
+++ app/(dashboard)/u/[username]/chat/_components/toggle-card.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { updateStream } from "@/actions/stream";
 import { Switch } from "@/components/ui/switch";
 
 type FieldTypes = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly";
@@ -15,12 +18,28 @@ export const ToggleCard = ({
   value = false,
   field,
 }: ToggleCardProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const onChange = async () => {
+    startTransition(() => {
+      updateStream({ [field]: !value })
+        .then(() => toast.success("Chat settings updated!"))
+        .catch(() => toast.error("Something went wrong!"));
+    });
+  };
+
   return (
     <div className="rounded-xl bg-muted p-6">
       <div className="flex items-center justify-between">
         <p className="font-semibold shrink-0">{label}</p>
         <div className="space-y-2">
-          <Switch checked={value}>{value ? "On" : "Off"}</Switch>
+          <Switch
+            disabled={isPending}
+            onCheckedChange={onChange}
+            checked={value}
+          >
+            {value ? "On" : "Off"}
+          </Switch>
         </div>
       </div>
     </div>
