f23fbb1d6160faf319a6da4cc0efa6e3bf65cbf7
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-02 20:07:39 +0500

    Connect Modal Comp

---
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertTriangle } from "lucide-react";
+
+export const ConnectModal = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"primary"}>Generate Connection</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Generate Connection</DialogTitle>
+        </DialogHeader>
+        <Alert>
+          <AlertTriangle className="h-4 w-4" />
+          <AlertTitle>Warning!</AlertTitle>
+          <AlertDescription>
+            This action will reset all active streams using the current
+            connection
+          </AlertDescription>
+        </Alert>
+        <div className="flex justify-between">
+          <DialogClose>
+            <Button variant={"ghost"}>Cancel</Button>
+          </DialogClose>
+          <Button onClick={() => {}} variant={"primary"}>
+            Generate
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
