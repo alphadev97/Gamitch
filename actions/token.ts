aefaae80e96f4bb95d3ed8eab09c1f0f3f911271
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-03 01:27:22 +0500

    createViewerToken

--- actions/token.ts
+++ actions/token.ts
@@ -3,3 +3,8 @@
 import { v4 } from "uuid";
 import { AccessToken } from "livekit-server-sdk";
 import { getSelf } from "@/lib/auth-service";
+import { getUserById } from "@/lib/user-service";
+
+export const createViewerToken = async (hostIdentity: string) => {
+  let self;
+};
