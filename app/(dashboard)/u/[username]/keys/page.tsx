9d0328af2d39eedc690fb1ab04c7260b0f568c36
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-02 17:46:29 +0500

    KeysPage

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -1,5 +1,15 @@
+import { Button } from "@/components/ui/button";
+
 const KeysPage = () => {
-  return <div>Keys Page</div>;
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-2xl font-bold">Keys & URLs</h1>
+        <Button variant={"primary"}>Generate</Button>
+      </div>
+      <div className="space-y-4"></div>
+    </div>
+  );
 };
 
 export default KeysPage;
