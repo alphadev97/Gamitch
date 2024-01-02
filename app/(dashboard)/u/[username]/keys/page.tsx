122dcc2960dbb19f401cec0a52f955b4042b5939
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-02 18:04:25 +0500

    Keys page, get user stream data

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -1,6 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { UrlCard } from "./_components/url-card";
+import { getSelf } from "@/lib/auth-service";
+import { getStreamByUserId } from "@/lib/stream-service";
 
-const KeysPage = () => {
+const KeysPage = async () => {
+  const self = await getSelf();
+  const stream = await getStreamByUserId(self.id);
+
+  if (!stream) {
+    throw new Error("Stream Not Found!");
+  }
+
   return (
     <div className="p-6">
       <div className="flex items-center justify-between mb-4">
@@ -8,7 +18,7 @@ const KeysPage = () => {
         <Button variant={"primary"}>Generate</Button>
       </div>
       <div className="space-y-4">
-        <UrlCard />
+        <UrlCard value={stream.serverUrl} />
       </div>
     </div>
   );
