e7bdd2a2f2eedfd336745726b8b9d393135b0d32
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-03 00:28:46 +0500

    home page

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,5 +1,21 @@
-const CreatorPage = () => {
-  return <div>CreatorPage</div>;
+import { getUserByUsername } from "@/lib/user-service";
+import { currentUser } from "@clerk/nextjs";
+
+interface CreatorPageProps {
+  params: {
+    username: string;
+  };
+}
+
+const CreatorPage = async ({ params }: CreatorPageProps) => {
+  const externalUser = await currentUser();
+  const user = await getUserByUsername(params.username);
+
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error("Unauthorized");
+  }
+
+  return <div className="h-full">CreatorPage</div>;
 };
 
 export default CreatorPage;
