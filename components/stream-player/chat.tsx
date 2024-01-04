71811ac48f4deeea5bd4a819e9cc198f680f3563
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-05 00:14:10 +0500

    Chat for stream player

---
+++ components/stream-player/chat.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+import {
+  useConnectionState,
+  useRemoteParticipant,
+  useChat,
+} from "@livekit/components-react";
+import { ConnectionState } from "livekit-client";
+import { useEffect, useMemo, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
+interface ChatProps {
+  hostName: string;
+  hostIdentity: string;
+  viewerName: string;
+  isFollowing: boolean;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+}
+
+export const Chat = ({
+  hostName,
+  hostIdentity,
+  viewerName,
+  isFollowing,
+  isChatEnabled,
+  isChatDelayed,
+  isChatFollowersOnly,
+}: ChatProps) => {
+  const matches = useMediaQuery("(max-width: 1024px)");
+  const { variant, onExpand } = useChatSidebar((state) => state);
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+
+  const isOnline = participant && connectionState === ConnectionState.Connected;
+
+  const isHidden = !isChatEnabled || !isOnline;
+
+  const [value, setValue] = useState("");
+  const { chatMessages: messages, send } = useChat();
+
+  useEffect(() => {
+    if (matches) {
+      onExpand();
+    }
+  }, [matches, onExpand]);
+
+  const reversedMessages = useMemo(() => {
+    return messages.sort((a, b) => b.timestamp - a.timestamp);
+  }, [messages]);
+
+  const onSubmit = () => {
+    if (!send) return;
+
+    send(value);
+    setValue("");
+  };
+
+  return <div>Chats</div>;
+};
