5e8f69adb88312ce394093044eef0aff5aa88e77
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-04 19:21:00 +0500

    live video comp

---
+++ components/stream-player/live-video.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Participant } from "livekit-client";
+
+interface LiveVideoProps {
+  participant: Participant;
+}
+
+export const LiveVideo = ({ participant }: LiveVideoProps) => {
+  return <div>LiveVideo</div>;
+};
