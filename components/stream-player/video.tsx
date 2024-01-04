28c1c085e10dfe6ac561d51393200b4e322b0ba8
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-04 19:02:26 +0500

    video player comp

--- components/stream-player/video.tsx
+++ components/stream-player/video.tsx
@@ -1,1 +1,34 @@
 "use client";
+
+import { ConnectionState, Track } from "livekit-client";
+import {
+  useConnectionState,
+  useRemoteParticipant,
+  useTracks,
+} from "@livekit/components-react";
+
+interface VideoProps {
+  hostName: string;
+  hostIdentity: string;
+}
+
+export const Video = ({ hostName, hostIdentity }: VideoProps) => {
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+  const tracks = useTracks([
+    Track.Source.Camera,
+    Track.Source.Microphone,
+  ]).filter((track) => track.participant.identity === hostIdentity);
+
+  let content;
+
+  if (!participant && connectionState === ConnectionState.Connected) {
+    content = <p>Host is offline!</p>;
+  } else if (!participant || tracks.length === 0) {
+    content = <p>Loading...</p>;
+  } else {
+    content = <p>Live video</p>;
+  }
+
+  return <div className="aspect-video border-b group relative">{content}</div>;
+};
