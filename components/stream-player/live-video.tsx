c76168ee45e01127f015328961405f9426252c80
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-04 20:23:41 +0500

    live video player comp

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,11 +1,28 @@
 "use client";
 
-import { Participant } from "livekit-client";
+import { Participant, Track } from "livekit-client";
+import { useRef } from "react";
+import { useTracks } from "@livekit/components-react";
 
 interface LiveVideoProps {
   participant: Participant;
 }
 
 export const LiveVideo = ({ participant }: LiveVideoProps) => {
-  return <div>LiveVideo</div>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useTracks([Track.Source.Camera, Track.Source.Microphone])
+    .filter((track) => track.participant.identity === participant.identity)
+    .forEach((track) => {
+      if (videoRef.current) {
+        track.publication.track?.attach(videoRef.current);
+      }
+    });
+
+  return (
+    <div ref={wrapperRef} className="relative h-full flex">
+      <video ref={videoRef} width={"100%"} />
+    </div>
+  );
 };
