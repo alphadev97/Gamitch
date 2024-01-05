546c2406080f4233c1910d66518a6d742134e689
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-05 19:22:29 +0500

    Chat Toggle

--- components/stream-player/chat-header.tsx
+++ components/stream-player/chat-header.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { ChatToggle } from "./chat-toggle";
 
 export const ChatHeader = () => {
   return (
     <div className="relative p-3 border-b">
-      {/* TODO: Toggle chat sidebar */}
+      <div className="absolute left-2 top-2 hidden lg:block">
+        <ChatToggle />
+      </div>
       <p className="font-semibold text-primary text-center">Stream chat</p>
       {/* TODO: Toggle chat community */}
     </div>
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -7,6 +7,7 @@ import { Stream, User } from "@prisma/client";
 import { LiveKitRoom } from "@livekit/components-react";
 import { Video } from "./video";
 import { Chat } from "./chat";
+import { ChatToggle } from "./chat-toggle";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -29,6 +30,12 @@ export const StreamPlayer = ({
 
   return (
     <>
+      {collapsed && (
+        <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+          <ChatToggle />
+        </div>
+      )}
+
       <LiveKitRoom
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
