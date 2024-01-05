566d058258eb5331bed885f623e369ef5d789265
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-05 23:56:18 +0500

    Chat message

--- components/stream-player/chat-list.tsx
+++ components/stream-player/chat-list.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ReceivedChatMessage } from "@livekit/components-react";
+import { ChatMessage } from "./chat-message";
 
 interface ChatListProps {
   messages: ReceivedChatMessage[];
@@ -19,6 +20,10 @@ export const ChatList = ({ messages, isHidden }: ChatListProps) => {
   }
 
   return (
-    <div className="flex flex-1 flex-col-reverse overflow-y-auto p-3 h-full"></div>
+    <div className="flex flex-1 flex-col-reverse overflow-y-auto p-3 h-full">
+      {messages.map((message) => (
+        <ChatMessage key={message.timestamp} data={message} />
+      ))}
+    </div>
   );
 };
