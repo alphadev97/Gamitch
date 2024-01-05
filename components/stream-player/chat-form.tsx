d22a06ec959760acd984a6cff635d67dae0da0e8
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-05 22:56:45 +0500

    Chat form skeleton

--- components/stream-player/chat-form.tsx
+++ components/stream-player/chat-form.tsx
@@ -87,6 +87,10 @@ export const ChatFormSkeleton = () => {
   return (
     <div className="flex flex-col items-center gap-y-4 p-3">
       <Skeleton className="w-full h-10" />
+      <div className="flex items-center gap-x-2 ml-auto">
+        <Skeleton className="h-7 w-7" />
+        <Skeleton className="h-7 w-12" />
+      </div>
     </div>
   );
 };
