1f10d0baa93fe809c3f370680a3413792c74b23b
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-05 23:10:58 +0500

    Chat info

--- components/stream-player/chat-form.tsx
+++ components/stream-player/chat-form.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
+import { ChatInfo } from "./chat-info";
 
 interface ChatFormProps {
   onSubmit: () => void;
@@ -58,6 +59,7 @@ export const ChatForm = ({
       className="flex flex-col items-center gap-y-4 p-3"
     >
       <div className="w-full">
+        <ChatInfo isDelayed={isDelayed} isFollowersOnly={isFollowersOnly} />
         <Input
           onChange={(e) => onChange(e.target.value)}
           value={value}
