bc4cd670c96375fbde91b8767e0832f7037e975b
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-05 23:58:28 +0500

    chat message date

--- components/stream-player/chat-message.tsx
+++ components/stream-player/chat-message.tsx
@@ -13,9 +13,7 @@ export const ChatMessage = ({ data }: ChatMessageProps) => {
 
   return (
     <div className="flex gap-2 p-2 rounded-md hover:bg-white/5">
-      <p className="text-sm text-white/40">
-        {format(data.timestamp, "HH: MM")}
-      </p>
+      <p className="text-sm text-white/40">{format(data.timestamp, "HH:MM")}</p>
       <div className="flex flex-wrap items-baseline gap-1 grow">
         <p className="text-sm font-semibold whitespace-nowrap">
           <span className="truncate" style={{ color: color }}>
