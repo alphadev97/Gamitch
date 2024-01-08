ee59309577a9688a46ab42c9676ed02cdcd78516
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-08 17:13:51 +0500

    chat community

--- components/stream-player/chat-community.tsx
+++ components/stream-player/chat-community.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { useParticipants } from "@livekit/components-react";
+import { useState } from "react";
+import { useDebounce } from "usehooks-ts";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
 interface ChatCommunityProps {
   hostName: string;
   viewerName: string;
@@ -11,5 +17,40 @@ export const ChatCommunity = ({
   viewerName,
   isHidden,
 }: ChatCommunityProps) => {
-  return <div></div>;
+  const [value, setValue] = useState("");
+  const debounceValue = useDebounce<string>(value, 500);
+
+  const participants = useParticipants();
+
+  const onChange = (newValue: string) => {
+    setValue(newValue);
+  };
+
+  if (isHidden) {
+    return (
+      <div className="flex flex-1 items-center justify-center">
+        <p className="text-sm text-muted-foreground">Community is diabled</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4">
+      <Input
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Search community"
+        className="border-white/10"
+      />
+      <ScrollArea className="gap-y-2 mt-4">
+        <p className="text-center text-sm text-muted-foreground hidden last:block p-2">
+          No results
+        </p>
+        {
+            participants.map((partcipant) => (
+                <CommunityItem />
+            ))
+        }
+      </ScrollArea>
+    </div>
+  );
 };
