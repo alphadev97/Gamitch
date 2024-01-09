331766133822906e133c4e41b724e727f20468c9
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-09 22:47:17 +0500

    uploadthing

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,33 @@
+import { getSelf } from "@/lib/auth-service";
+import { db } from "@/lib/db";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  thumbnailUploader: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(async () => {
+      const self = getSelf();
+
+      return { user: self };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      await db.stream.update({
+        where: {
+          userId: (await metadata.user).id, // i change
+        },
+        data: {
+          thumbnailUrl: file.url,
+        },
+      });
+
+      return { fileUrl: file.url };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
