4bdb22fc46039ed72d3cffda19d4c33a7403e0b8
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-09 23:43:51 +0500

    remove, update thumbnail

--- components/stream-player/info-modal.tsx
+++ components/stream-player/info-modal.tsx
@@ -36,6 +36,18 @@ export const InfoModal = ({
   const [name, setName] = useState(initialName);
   const [thumbnailUrl, setThumbnailUrl] = useState(initialThumbnailUrl);
 
+  const onRemove = () => {
+    startTransition(() => {
+      updateStream({ thumbnailUrl: null })
+        .then(() => {
+          toast.success("Thumbnail removed");
+          setThumbnailUrl("");
+          closeRef?.current?.click();
+        })
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -83,7 +95,7 @@ export const InfoModal = ({
                     <Button
                       type="button"
                       disabled={isPending}
-                      onClick={() => {}}
+                      onClick={onRemove}
                       className="h-auto w-auto p-1.5"
                     >
                       <Trash className="h-4 w-4" />
@@ -112,6 +124,7 @@ export const InfoModal = ({
                   onClientUploadComplete={(res) => {
                     setThumbnailUrl(res?.[0]?.url);
                     router.refresh();
+                    closeRef?.current?.click();
                   }}
                 />
               </div>
