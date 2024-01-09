81d81811a7bffd4f6e4d24332405b0f3692161e9
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-09 19:19:12 +0500

    Stream Info Card

--- components/stream-player/info-card.tsx
+++ components/stream-player/info-card.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Pencil } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
+
 interface InfoCardProps {
   name: string;
   thumbnailUrl: string | null;
@@ -13,5 +17,44 @@ export const InfoCard = ({
   hostIdentity,
   viewerIdentity,
 }: InfoCardProps) => {
-  return <div>Info Cards</div>;
+  const hostAsViewer = `host-${hostIdentity}`;
+  const isHost = viewerIdentity === hostAsViewer;
+
+  if (!isHost) return null;
+
+  return (
+    <div className="px-4">
+      <div className="rounded-xl bg-background">
+        <div className="flex items-center gap-x-2.5 p-4">
+          <div className="rounded-md bg-blue-600 p-2 h-auto w-auto">
+            <Pencil className="h-5 w-5" />
+          </div>
+          <div>
+            <h2 className="text-sm lg:text-lg font-semibold capitalize">
+              Edit your stream info
+            </h2>
+            <p className="text-muted-foreground text-xs lg:text-sm">
+              Maxmimize your visibility
+            </p>
+          </div>
+          {/* TODO: Add a model button */}
+        </div>
+        <Separator />
+        <div className="p-4 lg:p-6 space-y-4">
+          <div>
+            <h3 className="text-sm text-muted-foreground mb-2">Name</h3>
+            <p className="text-sm font-semibold">{name}</p>
+          </div>
+          <div>
+            <h3 className="text-sm text-muted-foreground mb-2">Thumbnail</h3>
+            {thumbnailUrl && (
+              <div className="relative aspect-video rounded-md overflow-hidden w-[200px] border border-white/10">
+                <Image fill src={thumbnailUrl} alt={name} />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
