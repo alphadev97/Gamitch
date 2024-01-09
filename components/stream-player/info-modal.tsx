b5e7c80dc2e565db7a8ea258d2951d38cd16e8f6
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-09 22:55:52 +0500

    uploadthing file upload

--- components/stream-player/info-modal.tsx
+++ components/stream-player/info-modal.tsx
@@ -8,22 +8,30 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { UploadDropzone } from "@/lib/uploadthing";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { useState, useTransition, useRef, ElementRef } from "react";
 import { updateStream } from "@/actions/stream";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 interface InfoModalProps {
   initialName: string;
   initialThumbnailUrl: string | null;
 }
 
-export const InfoModal = ({ initialName }: InfoModalProps) => {
+export const InfoModal = ({
+  initialName,
+  initialThumbnailUrl,
+}: InfoModalProps) => {
+  const router = useRouter();
   const closeRef = useRef<ElementRef<"button">>(null);
   const [isPending, startTransition] = useTransition();
+
   const [name, setName] = useState(initialName);
+  const [thumbnailUrl, setThumbnailUrl] = useState(initialThumbnailUrl);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -62,6 +70,26 @@ export const InfoModal = ({ initialName }: InfoModalProps) => {
               value={name}
               disabled={isPending}
             />
+          </div>
+          <div className="space-y-2">
+            <Label>Thumbnail</Label>
+            <div className="rounded-xl border outline-dashed outline-muted">
+              <UploadDropzone
+                endpoint="thumbnailUploader"
+                appearance={{
+                  label: {
+                    color: "#FFFFFF",
+                  },
+                  allowedContent: {
+                    color: "FFFFFF",
+                  },
+                }}
+                onClientUploadComplete={(res) => {
+                  setThumbnailUrl(res?.[0]?.url);
+                  router.refresh();
+                }}
+              />
+            </div>
           </div>
           <div className="flex justify-between">
             <DialogClose ref={closeRef} asChild>
