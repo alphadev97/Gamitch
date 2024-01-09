8d8d5b2fd29264151b43ee38c333198dd579c0c8
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-10 01:26:03 +0500

    Streamer about card

---
+++ components/stream-player/about-card.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { VarifiedMark } from "@/components/varified-mark";
+
+interface AboutCardProps {
+  hostName: string;
+  hostIdentity: string;
+  viewerIdentity: string;
+  bio: string | null;
+  followedByCount: number;
+}
+
+export const AboutCard = ({
+  hostName,
+  hostIdentity,
+  viewerIdentity,
+  bio,
+  followedByCount,
+}: AboutCardProps) => {
+  const hostAsViewer = `host-${hostIdentity}`;
+  const isHost = viewerIdentity === hostAsViewer;
+
+  const followedByLabel = followedByCount === 1 ? "follower" : "followers";
+
+  return (
+    <div className="px-4">
+      <div className="group rounded-xl bg-background p-6 lg:p-10 flex flex-col gap-y-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-x-2 font-semibold text-lg lg:text-2xl">
+            About {hostName}
+            <VarifiedMark />
+          </div>
+          {isHost && <p>EDIT</p>}
+        </div>
+        <div className="text-sm text-muted-foreground">
+          <span className="font-semibold text-primary">{followedByCount}</span>{" "}
+          {followedByLabel}
+        </div>
+        <p className="text-sm">{bio || "User has no bio"}</p>
+      </div>
+    </div>
+  );
+};
