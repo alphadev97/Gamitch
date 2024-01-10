0197c9bba8a2bed14727fc39fddfc26b679a5134
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-11 01:37:53 +0500

    Home page

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,9 +1,12 @@
-import { Results } from "./_components/results";
+import { Suspense } from "react";
+import { Results,ResultsSkeleton } from "./_components/results";
 
 export default function Page() {
   return (
     <div className="h-full p-8 max-w-screen-2xl mx-auto">
-      <Results />
+      <Suspense fallback={<ResultsSkeleton/>}>
+        <Results />
+      </Suspense>
     </div>
   );
 }
