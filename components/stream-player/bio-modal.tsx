826573edfa8b2632bb55a89dd8e4d395a35a4880
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-10 19:32:12 +0500

    Bio Modal

--- components/stream-player/bio-modal.tsx
+++ components/stream-player/bio-modal.tsx
@@ -1,5 +1,34 @@
 "use client";
 
-export const BioModal = () => {
-  return <div>EDIT</div>;
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Hint } from "@/components/hint";
+import { Textarea } from "@/components/ui/textarea";
+
+interface BioModalProps {
+  initialValue: string | null;
+}
+
+export const BioModal = ({ initialValue }: BioModalProps) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"link"} size={"sm"} className="ml-auto">
+          Edit
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit user bio</DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 };
