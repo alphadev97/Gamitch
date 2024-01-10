aa17ce5558a07aca93be2d8f9508845d6230409e
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-10 17:47:12 +0500

    Bio Modal

--- components/stream-player/about-card.tsx
+++ components/stream-player/about-card.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { VarifiedMark } from "@/components/varified-mark";
+import { BioModal } from "./bio-modal";
 
 interface AboutCardProps {
   hostName: string;
@@ -30,7 +31,7 @@ export const AboutCard = ({
             About {hostName}
             <VarifiedMark />
           </div>
-          {isHost && <p>EDIT</p>}
+          {isHost && <BioModal initialValue={bio} />}
         </div>
         <div className="text-sm text-muted-foreground">
           <span className="font-semibold text-primary">{followedByCount}</span>{" "}
---
+++ components/stream-player/bio-modal.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export const BioModal = () => {
+  return <div>EDIT</div>;
+};
