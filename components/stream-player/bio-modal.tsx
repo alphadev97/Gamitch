23193c1d03385233b6d76c8af3b0d6e280b69138
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-10 22:28:33 +0500

    bio modal

--- components/stream-player/bio-modal.tsx
+++ components/stream-player/bio-modal.tsx
@@ -9,14 +9,33 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { Hint } from "@/components/hint";
 import { Textarea } from "@/components/ui/textarea";
+import { useState, useTransition, useRef, ElementRef } from "react";
+import { updateUser } from "@/actions/user";
+import { toast } from "sonner";
 
 interface BioModalProps {
   initialValue: string | null;
 }
 
 export const BioModal = ({ initialValue }: BioModalProps) => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+  const [isPending, startTransition] = useTransition();
+  const [value, setValue] = useState(initialValue || "");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    startTransition(() => {
+      updateUser({ bio: value })
+        .then(() => {
+          toast.success("User bio updated!");
+          closeRef.current?.click();
+        })
+        .catch(() => toast.error("Something went wrong!"));
+    });
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -28,6 +47,25 @@ export const BioModal = ({ initialValue }: BioModalProps) => {
         <DialogHeader>
           <DialogTitle>Edit user bio</DialogTitle>
         </DialogHeader>
+        <form onSubmit={onSubmit} className="space-y-4">
+          <Textarea
+            placeholder="User bio"
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+            disabled={isPending}
+            className="resize-none"
+          />
+          <div className="flex justify-between">
+            <DialogClose ref={closeRef} asChild>
+              <Button type="button" variant={"ghost"}>
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button disabled={isPending} type="submit" variant={"primary"}>
+              Save
+            </Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
