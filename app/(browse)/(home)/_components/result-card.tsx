776bf711a82051edf6f7c7f90d70c333dfe757a8
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-11 20:00:30 +0500

    ResultCardSkeleton

--- app/(browse)/(home)/_components/result-card.tsx
+++ app/(browse)/(home)/_components/result-card.tsx
@@ -1,6 +1,7 @@
 import { LiveBadge } from "@/components/live-badge";
-import { Thumbnail } from "@/components/thumbnail";
-import { UserAvatar } from "@/components/user-avatar";
+import { Thumbnail, ThumbnailSkeleton } from "@/components/thumbnail";
+import { Skeleton } from "@/components/ui/skeleton";
+import { UserAvatar, UserAvatarSkeleton } from "@/components/user-avatar";
 import { Stream, User } from "@prisma/client";
 import Link from "next/link";
 
@@ -38,5 +39,20 @@ export const ResultCard = ({ data }: ResultCardProps) => {
         </div>
       </div>
     </Link>
+  );
+};
+
+export const ResultCardSkeleton = () => {
+  return (
+    <div className="h-full w-full space-y-4">
+      <ThumbnailSkeleton />
+      <div className="flex gap-x-3">
+        <UserAvatarSkeleton />
+        <div className="flex flex-col gap-y-1">
+          <Skeleton className="h-4 w-32" />
+          <Skeleton className="h-3 w-24" />
+        </div>
+      </div>
+    </div>
   );
 };
