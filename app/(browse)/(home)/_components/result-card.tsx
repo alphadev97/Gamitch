81523091329408cdddd8d590ac542c3debf39dc3
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-11 19:51:54 +0500

    result card

--- app/(browse)/(home)/_components/result-card.tsx
+++ app/(browse)/(home)/_components/result-card.tsx
@@ -1,4 +1,6 @@
+import { LiveBadge } from "@/components/live-badge";
 import { Thumbnail } from "@/components/thumbnail";
+import { UserAvatar } from "@/components/user-avatar";
 import { Stream, User } from "@prisma/client";
 import Link from "next/link";
 
@@ -16,6 +18,24 @@ export const ResultCard = ({ data }: ResultCardProps) => {
           isLive={data.isLive}
           username={data.user.username}
         />
+        {data.isLive && (
+          <div className="absolute top-2 left-2 group-hover:translate-x-2 group-hover:-translate-y-2 transition-transform">
+            <LiveBadge />
+          </div>
+        )}
+        <div className="flex gap-3">
+          <UserAvatar
+            username={data.user.username}
+            imageUrl={data.user.imageUrl}
+            isLive={data.isLive}
+          />
+          <div className="flex flex-col text-sm overflow-hidden">
+            <p className="truncate font-semibold hover:text-blue-500">
+              {data.name}
+            </p>
+            <p className="text-muted-foreground">{data.user.username}</p>
+          </div>
+        </div>
       </div>
     </Link>
   );
