72bea7f3ce42c5961835a19cc8eab8968fe53a28
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-11 20:00:40 +0500

    ResultsSkeleton

--- app/(browse)/(home)/_components/results.tsx
+++ app/(browse)/(home)/_components/results.tsx
@@ -1,5 +1,6 @@
 import { getStreams } from "@/lib/feed-service";
-import { ResultCard } from "./result-card";
+import { ResultCard, ResultCardSkeleton } from "./result-card";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Results = async () => {
   const data = await getStreams();
@@ -22,5 +23,14 @@ export const Results = async () => {
 };
 
 export const ResultsSkeleton = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Skeleton className="h-8 w-[290px] mb-4" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+        {[...Array(4)].map((_, i) => (
+          <ResultCardSkeleton key={i} />
+        ))}
+      </div>
+    </div>
+  );
 };
