151d00ff9de35539ac5e797f1b9e8b586e86a949
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-12 01:12:17 +0500

    Security issue

--- app/(browse)/search/_components/result-card.tsx
+++ app/(browse)/search/_components/result-card.tsx
@@ -1,12 +1,19 @@
 import { Thumbnail, ThumbnailSkeleton } from "@/components/thumbnail";
-import { Stream, User } from "@prisma/client";
+import { User } from "@prisma/client";
 import Link from "next/link";
 import { VarifiedMark } from "@/components/varified-mark";
 import { formatDistanceToNow } from "date-fns";
 import { Skeleton } from "@/components/ui/skeleton";
 
 interface ResultCardProps {
-  data: Stream & { user: User };
+  data: {
+    id: string;
+    name: string;
+    thumbnailUrl: string | null;
+    isLive: boolean;
+    updatedAt: Date;
+    user: User;
+  };
 }
 
 export const ResultCard = ({ data }: ResultCardProps) => {
