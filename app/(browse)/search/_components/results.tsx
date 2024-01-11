d7d01a0b444bf002e848c731010bea9514d2bbc1
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-11 23:29:36 +0500

    Search result

---
+++ app/(browse)/search/_components/results.tsx
@@ -0,0 +1,17 @@
+interface ResultsProps {
+  term?: string;
+}
+
+export const Results = ({ term }: ResultsProps) => {
+  return (
+    <div>
+      <h2 className="text-lg font-semibold mb-4">
+        Results for term &quot;{term}&quot;
+      </h2>
+    </div>
+  );
+};
+
+export const ResultsSkeleton = () => {
+  return <div></div>;
+};
