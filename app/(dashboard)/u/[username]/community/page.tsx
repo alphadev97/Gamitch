b68d14b80a166fd9ffd5376f7ae7aa9f4df6f61a
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-01-12 01:24:27 +0500

    Community page

---
+++ app/(dashboard)/u/[username]/community/page.tsx
@@ -0,0 +1,29 @@
+import { Payment, columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+
+async function getData(): Promise<Payment[]> {
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+const CommunityPage = async () => {
+  const data = await getData();
+
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-bold">Community Settings</h1>
+      </div>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+};
+
+export default CommunityPage;
